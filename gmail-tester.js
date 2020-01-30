d6808a30e1f853d697289a2a80a7d08efd7c1e40
kateyurasova <kateyurasova@gmail.com>
2020-01-30 17:40:42 +0300

    Added fuctions to reply google emails with the appropriate "subject from to" combination

--- gmail-tester.js
+++ gmail-tester.js
@@ -1,178 +1,193 @@
 const gmail = require("./gmail");
 const fs = require("fs");
-const { google } = require("googleapis");
+const {google} = require("googleapis");
 const util = require("util");
 
 function _get_header(name, headers) {
-  const found = headers.find(h => h.name === name);
-  return found && found.value;
+    const found = headers.find(h => h.name === name);
+    return found && found.value;
 }
 
 function _init_query(options) {
-  const { to, from, subject, before, after } = options;
-  let query = "";
-  if (to) {
-    query += `to:"${to}" `;
-  }
-  if (from) {
-    query += `from:"${from}" `;
-  }
-  if (subject) {
-    query += `subject:(${subject}) `;
-  }
-  if (after) {
-    const after_epoch = Math.round(new Date(after).getTime() / 1000);
-    query += `after:${after_epoch} `;
-  }
-  if (before) {
-    const before_epoch = Math.round(new Date(before).getTime() / 1000);
-    query += `before:${before_epoch} `;
-  }
-  query = query.trim();
-  return query;
+    const {to, from, subject, before, after} = options;
+    let query = "";
+    if (to) {
+        query += `to:"${to}" `;
+    }
+    if (from) {
+        query += `from:"${from}" `;
+    }
+    if (subject) {
+        query += `subject:(${subject}) `;
+    }
+    if (after) {
+        const after_epoch = Math.round(new Date(after).getTime() / 1000);
+        query += `after:${after_epoch} `;
+    }
+    if (before) {
+        const before_epoch = Math.round(new Date(before).getTime() / 1000);
+        query += `before:${before_epoch} `;
+    }
+    query = query.trim();
+    return query;
 }
 
 async function _get_recent_email(credentials_json, token_path, options = {}) {
-  const emails = [];
+    const emails = [];
 
-  const query = _init_query(options);
-  // Load client secrets from a local file.
-  const content = fs.readFileSync(credentials_json);
-  const oAuth2Client = await gmail.authorize(JSON.parse(content), token_path);
-  const gmail_client = google.gmail({ version: "v1", oAuth2Client });
-  const gmail_emails = await gmail.get_recent_email(
-    gmail_client,
-    oAuth2Client,
-    query
-  );
-  for (const gmail_email of gmail_emails) {
-    const email = {
-      from: _get_header("From", gmail_email.payload.headers),
-      subject: _get_header("Subject", gmail_email.payload.headers),
-      receiver: _get_header("Delivered-To", gmail_email.payload.headers),
-      date: new Date(+gmail_email["internalDate"])
-    };
-    if (options.include_body) {
-      let email_body = {
-        html: "",
-        text: ""
-      };
-      const { body } = gmail_email.payload;
-      if (body.size) {
-        switch (gmail_email.payload.mimeType) {
-          case "text/html":
-            email_body.html = Buffer.from(body.data, "base64").toString("utf8");
-            break;
-          case "text/plain":
-          default:
-            email_body.text = Buffer.from(body.data, "base64").toString("utf8");
-            break;
-        }
-      } else {
-        let body_part = gmail_email.payload.parts.find(
-          p => p.mimeType === "text/html"
-        );
-        if (body_part) {
-          email_body.html = Buffer.from(body_part.body.data, "base64").toString(
-            "utf8"
-          );
-        }
-        body_part = gmail_email.payload.parts.find(
-          p => p.mimeType === "text/plain"
-        );
-        if (body_part) {
-          email_body.text = Buffer.from(body_part.body.data, "base64").toString(
-            "utf8"
-          );
-        }
-      }
-
-      email.body = email_body;
-    }
-    emails.push(email);
-  }
-  return emails;
-}
-
-async function __check_inbox(credentials_json, token_path, options = {}) {
-  const { subject, from, to, wait_time_sec, max_wait_time_sec } = options;
-  try {
-    console.log(
-      `[gmail] Checking for message from '${from}', to: ${to}, contains '${subject}' in subject...`
-    );
+    const query = _init_query(options);
     // Load client secrets from a local file.
-    let found_email = null;
-    let done_waiting_time = 0;
-    do {
-      const emails = await _get_recent_email(
-        credentials_json,
-        token_path,
-        options
-      );
-      for (let email of emails) {
-        if (
-          email.receiver === to &&
-          email.subject.indexOf(subject) >= 0 &&
-          email.from.indexOf(from) >= 0
-        ) {
-          console.log(`[gmail] Found!`);
-          found_email = email;
-          break;
+    const content = fs.readFileSync(credentials_json);
+    const oAuth2Client = await gmail.authorize(JSON.parse(content), token_path);
+    const gmail_client = google.gmail({version: "v1", oAuth2Client});
+    const gmail_emails = await gmail.get_recent_email(
+        gmail_client,
+        oAuth2Client,
+        query
+    );
+    for (const gmail_email of gmail_emails) {
+        const email = {
+            from: _get_header("From", gmail_email.payload.headers),
+            subject: _get_header("Subject", gmail_email.payload.headers),
+            receiver: _get_header("Delivered-To", gmail_email.payload.headers),
+            threadId: gmail_email.threadId,
+            date: new Date(+gmail_email["internalDate"])
+        };
+        if (options.include_body) {
+            let email_body = {
+                html: "",
+                text: ""
+            };
+            const {body} = gmail_email.payload;
+            if (body.size) {
+                switch (gmail_email.payload.mimeType) {
+                    case "text/html":
+                        email_body.html = Buffer.from(body.data, "base64").toString("utf8");
+                        break;
+                    case "text/plain":
+                    default:
+                        email_body.text = Buffer.from(body.data, "base64").toString("utf8");
+                        break;
+                }
+            } else {
+                let body_part = gmail_email.payload.parts.find(
+                    p => p.mimeType === "text/html"
+            );
+                if (body_part) {
+                    email_body.html = Buffer.from(body_part.body.data, "base64").toString(
+                        "utf8"
+                    );
+                }
+                body_part = gmail_email.payload.parts.find(
+                    p => p.mimeType === "text/plain"
+            );
+                if (body_part) {
+                    email_body.text = Buffer.from(body_part.body.data, "base64").toString(
+                        "utf8"
+                    );
+                }
+            }
+
+            email.body = email_body;
         }
-      }
-      if (!found_email) {
-        console.log(
-          `[gmail] Message not found. Waiting ${wait_time_sec} seconds...`
-        );
-        done_waiting_time += wait_time_sec;
-        if (done_waiting_time >= max_wait_time_sec) {
-          console.log("[gmail] Maximum waiting time exceeded!");
-          break;
-        }
-        await util.promisify(setTimeout)(wait_time_sec * 1000);
-      }
-    } while (!found_email);
-    return found_email;
-  } catch (err) {
-    console.log("[gmail] Error:", err);
-    throw err;
-  }
+        emails.push(email);
+    }
+    return emails;
 }
 
-/**
- * Poll inbox.
- *
- * @param {string} credentials_json - Path to credentials json file.
- * @param {string} token_path - Path to token json file.
- * @param {Object} options
- * @param {boolean} options.include_body - Set to `true` to fetch decoded email bodies.
- * @param {boolean} options.from - Filter on the email address of the receiver.
- * @param {boolean} options.to - Filter on the email address of the sender.
- * @param {boolean} options.subject - Filter on the subject of the email.
- * @param {boolean} options.before - Date. Filter messages received _after_ the specified date.
- * @param {boolean} options.after - Date. Filter messages received _before_ the specified date.
- * @param {boolean} options.wait_time_sec - Interval between inbox checks (in seconds). Default: 30 seconds.
- * @param {boolean} options.max_wait_time_sec - Maximum wait time (in seconds). When reached and the email was not found, the script exits. Default: 60 seconds.
- */
-async function check_inbox(
-  credentials_json,
-  token_path,
-  options = {
-    subject: undefined,
-    from: undefined,
-    to: undefined,
-    wait_time_sec: 30,
-    max_wait_time_sec: 30,
-    include_body: false
-  }
-) {
-  if (typeof options !== "object") {
-    console.error(
-      "This functionality is absolete! Please pass all params in options object!"
+async function reply_email(credentials_json,
+                           token_path,
+                           subject,
+                           from,
+                           to,
+                           replyMessage,
+                           wait_time_sec = 30,
+                           max_wait_time_sec = 60,
+                           options = {}) {
+    const content = fs.readFileSync(credentials_json);
+    const oAuth2Client = await gmail.authorize(JSON.parse(content), token_path);
+    const gmail_client = google.gmail({version: "v1", oAuth2Client});
+    var foundEmail = await check_inbox(credentials_json,
+        token_path,
+        subject,
+        from,
+        to,
+        wait_time_sec,
+        max_wait_time_sec,
+        options)
+    const response = await gmail.reply(
+        gmail_client,
+        oAuth2Client,
+        foundEmail.threadId,
+        subject,
+        to,
+        from,
+        replyMessage
     );
-    process.exit(1);
-  }
-  return __check_inbox(credentials_json, token_path, options);
+    return response;
+}
+
+async function get_all_emails(credentials_json, token_path, options = {}) {
+    const query = _init_query(options);
+    const content = fs.readFileSync(credentials_json);
+    const oAuth2Client = await gmail.authorize(JSON.parse(content), token_path);
+    const gmail_client = google.gmail({version: "v1", oAuth2Client});
+    const gmail_emails = await gmail.get_recent_email(
+        gmail_client,
+        oAuth2Client,
+        query
+    );
+    return gmail_emails;
+}
+
+async function check_inbox(credentials_json,
+                           token_path,
+                           subject,
+                           from,
+                           to,
+                           wait_time_sec = 30,
+                           max_wait_time_sec = 60,
+                           options = {}) {
+    try {
+        console.log(
+            `[gmail] Checking for message from '${from}', to: ${to}, contains '${subject}' in subject...`
+        );
+        // Load client secrets from a local file.
+        let found_email = null;
+        let done_waiting_time = 0;
+        do {
+            const emails = await _get_recent_email(
+                credentials_json,
+                token_path,
+                options
+            );
+            for (let email of emails) {
+                if (
+                    email.receiver === to &&
+                    email.subject.indexOf(subject) >= 0 &&
+                    email.from.indexOf(from) >= 0
+                ) {
+                    console.log(`[gmail] Found!`);
+                    found_email = email;
+                    break;
+                }
+            }
+            if (!found_email) {
+                console.log(
+                    `[gmail] Message not found. Waiting ${wait_time_sec} seconds...`
+                );
+                done_waiting_time += wait_time_sec;
+                if (done_waiting_time >= max_wait_time_sec) {
+                    console.log("[gmail] Maximum waiting time exceeded!");
+                    break;
+                }
+                await util.promisify(setTimeout)(wait_time_sec * 1000);
+            }
+        } while (!found_email);
+        return found_email;
+    } catch (err) {
+        console.log("[gmail] Error:", err);
+    }
 }
 
 /**
@@ -184,19 +199,21 @@ async function check_inbox(
  * @param {boolean} options.include_body - Return message body string.
  */
 async function get_messages(credentials_json, token_path, options) {
-  try {
-    const emails = await _get_recent_email(
-      credentials_json,
-      token_path,
-      options
-    );
-    return emails;
-  } catch (err) {
-    console.log("[gmail] Error:", err);
-  }
+    try {
+        const emails = await _get_recent_email(
+            credentials_json,
+            token_path,
+            options
+        );
+        return emails;
+    } catch (err) {
+        console.log("[gmail] Error:", err);
+    }
 }
 
 module.exports = {
-  check_inbox,
-  get_messages
+    check_inbox,
+    get_messages,
+    get_all_emails,
+    reply_email
 };
--- gmail.js
+++ gmail.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
-const { google } = require("googleapis");
+const {google} = require("googleapis");
+const Base64 = require('js-base64').Base64;
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -16,22 +18,22 @@ const TOKEN_PATH = "token.json";
  * @param {Object} credentials The authorization client credentials.
  */
 async function authorize(credentials, token_path) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
-  // Check if we have previously stored a token.
-  try {
-    const token = fs.readFileSync(
-      token_path || path.resolve(__dirname, TOKEN_PATH)
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
     );
-    oAuth2Client.setCredentials(JSON.parse(token));
-    return oAuth2Client;
-  } catch (error) {
-    return await get_new_token(oAuth2Client, token_path);
-  }
+    // Check if we have previously stored a token.
+    try {
+        const token = fs.readFileSync(
+            token_path || path.resolve(__dirname, TOKEN_PATH)
+        );
+        oAuth2Client.setCredentials(JSON.parse(token));
+        return oAuth2Client;
+    } catch (error) {
+        return await get_new_token(oAuth2Client, token_path);
+    }
 }
 
 /**
@@ -41,32 +43,32 @@ async function authorize(credentials, token_path) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 async function get_new_token(oAuth2Client, token_path) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES
-  });
-  console.log("Authorize this app by visiting this url:", authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  return new Promise((resolve, reject) => {
-    rl.question("Enter the code from that page here: ", async code => {
-      rl.close();
-      oAuth2Client.getToken(code, function(err, token) {
-        if (err) {
-          reject(err);
-        } else {
-          oAuth2Client.setCredentials(token);
-          fs.writeFileSync(
-            token_path || path.resolve(__dirname, TOKEN_PATH),
-            JSON.stringify(token)
-          );
-          resolve(oAuth2Client);
-        }
-      });
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES
     });
-  });
+    console.log("Authorize this app by visiting this url:", authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    return new Promise((resolve, reject) => {
+        rl.question("Enter the code from that page here: ", async code => {
+        rl.close();
+        oAuth2Client.getToken(code, function (err, token) {
+            if (err) {
+                reject(err);
+            } else {
+                oAuth2Client.setCredentials(token);
+                fs.writeFileSync(
+                    token_path || path.resolve(__dirname, TOKEN_PATH),
+                    JSON.stringify(token)
+                );
+                resolve(oAuth2Client);
+            }
+        });
+    });
+});
 }
 
 /**
@@ -75,28 +77,28 @@ async function get_new_token(oAuth2Client, token_path) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 async function list_labels(gmail, oauth2Client) {
-  try {
-    const labels = await new Promise((resolve, reject) => {
-      gmail.users.labels.list(
-        {
-          userId: "me",
-          auth: oauth2Client
-        },
-        function(err, res) {
-          if (err) {
-            reject(err);
-          } else {
-            const labels = res.data.labels;
-            resolve(labels);
-          }
-        }
-      );
+    try {
+        const labels = await new Promise((resolve, reject) => {
+            gmail.users.labels.list(
+            {
+                userId: "me",
+                auth: oauth2Client
+            },
+            function (err, res) {
+                if (err) {
+                    reject(err);
+                } else {
+                    const labels = res.data.labels;
+                    resolve(labels);
+                }
+            }
+        );
     });
-    return labels;
-  } catch (err) {
-    console.log("The API returned an error: " + err);
-    throw err;
-  }
+        return labels;
+    } catch (err) {
+        console.log("The API returned an error: " + err);
+        throw err;
+    }
 }
 
 /**
@@ -107,49 +109,49 @@ async function list_labels(gmail, oauth2Client) {
  * @param  {String} query String used to filter the Messages listed.
  */
 async function list_messages(gmail, oauth2Client, query, labelIds) {
-  const messages = await new Promise((resolve, reject) => {
-    gmail.users.messages.list(
-      {
-        userId: "me",
-        q: query,
-        auth: oauth2Client,
-        labelIds: labelIds
-      },
-      async function(err, res) {
+    const messages = await new Promise((resolve, reject) => {
+        gmail.users.messages.list(
+        {
+            userId: "me",
+            q: query,
+            auth: oauth2Client,
+            labelIds: labelIds
+        },
+        async function (err, res) {
         if (err) {
-          reject(err);
+            reject(err);
         } else {
-          let result = res.data.messages || [];
-          let { nextPageToken } = res.data;
-          while (nextPageToken) {
-            const resp = await new Promise((resolve, reject) => {
-              gmail.users.messages.list(
-                {
-                  userId: "me",
-                  q: query,
-                  auth: oauth2Client,
-                  labelIds: labelIds,
-                  pageToken: nextPageToken
-                },
-                function(err, res) {
-                  if (err) {
-                    reject(err);
-                  } else {
-                    resolve(res);
-                  }
-                }
-              );
+            let result = res.data.messages || [];
+            let {nextPageToken} = res.data;
+            while (nextPageToken) {
+                const resp = await new Promise((resolve, reject) => {
+                    gmail.users.messages.list(
+                    {
+                        userId: "me",
+                        q: query,
+                        auth: oauth2Client,
+                        labelIds: labelIds,
+                        pageToken: nextPageToken
+                    },
+                    function (err, res) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(res);
+                        }
+                    }
+                );
             });
-            result = result.concat(resp.data.messages);
-            nextPageToken = resp.data.nextPageToken;
-          }
-          resolve(result);
+                result = result.concat(resp.data.messages);
+                nextPageToken = resp.data.nextPageToken;
+            }
+            resolve(result);
         }
-      }
-    );
-  });
-  let result = messages || [];
-  return result;
+    }
+);
+});
+    let result = messages || [];
+    return result;
 }
 
 /**
@@ -159,43 +161,105 @@ async function list_messages(gmail, oauth2Client, query, labelIds) {
  * @param {String} query String used to filter the Messages listed.
  */
 async function get_recent_email(gmail, oauth2Client, query = "") {
-  try {
-    const labels = await list_labels(gmail, oauth2Client);
-    const inbox_label_id = [labels.find(l => l.name === "INBOX").id];
-    const messages = await list_messages(
-      gmail,
-      oauth2Client,
-      query,
-      inbox_label_id
-    );
-    let promises = [];
-    for (let message of messages) {
-      promises.push(
-        new Promise((resolve, reject) => {
-          gmail.users.messages.get(
-            {
-              auth: oauth2Client,
-              userId: "me",
-              id: message.id,
-              format: "full"
-            },
-            function(err, res) {
-              if (err) {
-                reject(err);
-              } else {
-                resolve(res);
-              }
-            }
-          );
+    try {
+        const labels = await list_labels(gmail, oauth2Client);
+        const inbox_label_id = [labels.find(l => l.name === "INBOX").id];
+        const messages = await list_messages(
+            gmail,
+            oauth2Client,
+            query,
+            inbox_label_id
+        );
+        let promises = [];
+        for (let message of messages) {
+            promises.push(
+                new Promise((resolve, reject) => {
+                    gmail.users.messages.get(
+                {
+                    auth: oauth2Client,
+                    userId: "me",
+                    id: message.id,
+                    format: "full"
+                },
+                function (err, res) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(res);
+                    }
+                }
+                );
         })
-      );
+        );
+        }
+        const results = await Promise.all(promises);
+        return results.map(r => r.data);
+    } catch (error) {
     }
-    const results = await Promise.all(promises);
-    return results.map(r => r.data);
-  } catch (error) {}
+}
+
+async function createEmail(to, from, subject, message) {
+    let email = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
+        "MIME-Version: 1.0\n",
+        "Content-Transfer-Encoding: 7bit\n",
+        "to: ", to, "\n",
+        "from: ", from, "\n",
+        "subject: ", subject, "\n\n",
+        message
+    ].join('');
+
+    return Base64.encodeURI(email);
+}
+
+async function reply(gmail, oauth2Client, threadId, subject, from, to, message) {
+    try {
+        let promises = [];
+        // var to = 'enterprise@dcautotest.com';
+        // var from = 'drivecentricautomation@gmail.com';
+        // var subject = 'Enterprise User from DC Automated Testing 1';
+        //var message = 'Reply for Drive Centric';
+
+        var email = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
+            "MIME-Version: 1.0\n",
+            "Content-Transfer-Encoding: 7bit\n",
+            "to: ", to, "\n",
+            "from: ", from, "\n",
+            "subject: ", subject, "\n\n",
+            message
+        ].join('');
+
+        var base64EncodedEmail = Base64.encodeURI(email);
+
+        promises.push(
+            new Promise((resolve, reject) => {
+                gmail.users.messages.send(
+            {
+                auth: oauth2Client,
+                userId: "me",
+                resource: {
+                    raw: base64EncodedEmail,
+                    threadId : threadId
+                }
+            },
+            function (err, res) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            }
+            );
+    })
+    );
+        const result = await Promise.all(promises);
+        return result;
+    } catch (error) {
+        return 'Error ' + error
+    }
 }
 
 module.exports = {
-  authorize,
-  get_recent_email
+    authorize,
+    get_recent_email,
+    reply
 };
